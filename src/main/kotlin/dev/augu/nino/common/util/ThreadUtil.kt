3ed7a0c8cccaa6d1105d5fff7e5e575ef851ba6c
Oded Shapira <odedshapira1@protonmail.com>
2020-11-13 03:21:24 +0200

    cleaned the create thread method

--- src/main/kotlin/dev/augu/nino/common/util/ThreadUtil.kt
+++ src/main/kotlin/dev/augu/nino/common/util/ThreadUtil.kt
@@ -7,8 +7,4 @@ package dev.augu.nino.common.util
  * @param block The block to execute when the Thread is executed
  * @return A [java.lang.Thread] instance that will run when called.
  */
-fun createThread(name: String, block: () -> Unit): Thread = (object: Thread(name) {
-    override fun run() {
-        block()
-    }
-})
+fun createThread(name: String, block: () -> Unit): Thread = Thread(block, name)
